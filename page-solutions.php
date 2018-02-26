14f832eea6873b2ec29875f6021b9efcb41789a9
Frost <cpaccione77@gmail.com>
2018-02-26 15:18:35 -0600

    Solutions page

---
+++ page-solutions.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+Template Name: Solutions
+
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<section class="solutions-hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<?php the_title('<h1>', '</h1>'); ?>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
