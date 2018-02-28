295de9b10881f34b823016b24fde7701bbf241a8
Frost <cpaccione77@gmail.com>
2018-02-27 23:29:29 -0600

    solutions page

--- page-solutions.php
+++ page-solutions.php
@@ -21,4 +21,43 @@ Template Name: Solutions
 	</div>
 </section>
 
+<section class="home-bg-one"></section>
+
+  <section class="home-solutions">
+    <div class="container">
+      <div class="row icon-row">
+        <div class="col-sm-4">
+          <div class="wrap">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/heart-handshake-icon.svg" alt="Eclispce Global Resources heart handshake icon">
+            <div class="copy-wrap">
+              <h3>Staffing Solutions</h3>
+              <p>Consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum.</p>
+              <a class="link-button" href="#">learn more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="wrap">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/gear-icon.svg" alt="Eclipse Global Resources gear icon">
+            <div class="copy-wrap">
+              <h3>Service Solutions</h3>
+              <p>Consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum.</p>
+              <a class="link-button" href="#">learn more</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="wrap">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/lightbulb-icon.svg" alt="Eclipse Global Resources lightbulb icon">
+            <div class="copy-wrap">
+              <h3>Strategic Solutions</h3>
+              <p>Consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum.</p>
+              <a class="link-button" href="#">learn more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 <?php get_footer(); ?>
