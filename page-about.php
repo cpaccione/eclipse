d27f7a28fc021987dff172d83ff4b12e77d1d3df
Chris Paccione <cpaccione77@gmail.com>
2018-03-19 00:38:29 -0500

    about page
    - continued work on the about page by adding remaining sections

--- page-about.php
+++ page-about.php
@@ -10,34 +10,74 @@ Template Name: About Page
 
 <?php get_header(); ?>
 
-<header class="single-solutions-hero">
+<header class="about">
 	<div class="container">
 		<div class="row">
-			<div class="col-8">
-				<h1>about</h1>
+			<div class="col-md-7">
 				<div class="wrap">
+					<h1>about</h1>
 					<p class="sub-sections">We offer and support contract, contract-to-hire, and direct hire staffing needs.</p>
 					<p>Eclipse is a Small-Women owned business with Executive staff providing a combined 25+ years of experience in the wireless and technology industries. Our range includes staffing, services, and vendor management with a focus on IT, Engineering, and Project/Program Management.</p>
 					<p>We offer flexible solutions and place significant emphasis on automation to keep our rates and fees in line with the industry (or lower). We understand and agree that each business is unique and will work hard to not only earn, but maintain your business.</p>
 				</div>
 			</div>
-			<div class="col-4">
-				<img src="<?php echo esc_url('get_template_directory_uri'); ?>/images/sba_wosb_vertical.svg" alt="SBA Women owned business logo">
+			<div class="col-md-4 d-flex align-items-center">
+				<img class="mx-auto d-block" src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/sba_wosb_vertical.svg" alt="SBA Women owned business logo">
 			</div>
 		</div>
 	</div>
 </header>
 
 <div class="meet-the-team">
-
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2>meet the team</h2>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="our-partners">
-
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2>our partners</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/verizon.svg" alt="Verizon logo" />
+			</div>
+			<div class="col-sm-4">
+<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/samsung.svg" alt="Verizon logo" />
+			</div>
+			<div class="col-sm-4">
+<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/sprint.svg" alt="Verizon logo" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/extenet.svg" alt="Verizon logo" />
+			</div>
+			<div class="col-sm-4">
+<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/teleworld.svg" alt="Verizon logo" />
+			</div>
+			<div class="col-sm-4">
+<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/crowncastle.svg" alt="Verizon logo" />
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="highlights">
-
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2>success highlights</h2>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
