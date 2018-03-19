1bb42ac6344a0e11d8b75ad2c31501c164e3aba2
Frost <cpaccione77@gmail.com>
2018-03-19 00:07:41 -0500

    about-template

---
+++ page-about.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+
+Template Name: About Page
+
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<header class="single-solutions-hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-8">
+				<h1>about</h1>
+				<div class="wrap">
+					<p class="sub-sections">We offer and support contract, contract-to-hire, and direct hire staffing needs.</p>
+					<p>Eclipse is a Small-Women owned business with Executive staff providing a combined 25+ years of experience in the wireless and technology industries. Our range includes staffing, services, and vendor management with a focus on IT, Engineering, and Project/Program Management.</p>
+					<p>We offer flexible solutions and place significant emphasis on automation to keep our rates and fees in line with the industry (or lower). We understand and agree that each business is unique and will work hard to not only earn, but maintain your business.</p>
+				</div>
+			</div>
+			<div class="col-4">
+				<img src="<?php echo esc_url('get_template_directory_uri'); ?>/images/sba_wosb_vertical.svg" alt="SBA Women owned business logo">
+			</div>
+		</div>
+	</div>
+</header>
+
+<div class="meet-the-team">
+
+</div>
+
+<div class="our-partners">
+
+</div>
+
+<div class="highlights">
+
+</div>
+
+<?php get_footer(); ?>
