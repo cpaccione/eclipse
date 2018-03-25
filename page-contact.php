d996d8cdf5b4789a677624721ac6f221f7fa08e7
Chris Paccione <cpaccione77@gmail.com>
2018-03-25 17:39:38 -0500

    Contact page
    Added loop to contact page for contact form 7

--- page-contact.php
+++ page-contact.php
@@ -18,6 +18,25 @@ Template Name: Contact
 				<p>Have any questions or comments? Please get in touch. We'd love to hear from you!</p>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-12">
+
+			    <?php if ( have_posts() ) :
+
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+
+					the_content();
+					
+					endwhile;
+
+				endif;
+
+
+				?>
+
+			</div>
+		</div>
 	</div>
 </section>
 
