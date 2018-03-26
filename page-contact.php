7b307e976bfdad0e65817e38de82e98511f8b130
Frost <cpaccione77@gmail.com>
2018-03-26 09:08:41 -0500

    Contact Form 7

--- page-contact.php
+++ page-contact.php
@@ -25,20 +25,20 @@ Template Name: Contact
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-
-			    <?php if ( have_posts() ) :
+				<div class="wrap">
+					<?php if ( have_posts() ) :
 
 					/* Start the Loop */
 					while ( have_posts() ) : the_post();
 
 					the_content();
-					
+
 					endwhile;
 
 				endif;
 
 				?>
-
+				</div>
 			</div>
 		</div>
 	</div>
