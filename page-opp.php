74c5aa6a3f3b9d1e843fde34679ce48dbeb60ad1
Chris Paccione <cpaccione77@gmail.com>
2018-03-28 01:18:20 -0500

    Updated copy

--- page-opp.php
+++ page-opp.php
@@ -15,7 +15,10 @@ Template Name: Opportunities
 		<div class="row">
 			<div class="col-12">
 				<?php the_title('<h1>', '</h1>'); ?>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                <h4>We'd love to hear from you!</h4>
+				<p>When you apply for a job opening with Eclipse, you’re getting the attention you deserve. Whether or not you’re chosen for the opening, we will continue to work with you to find a spot for you!</p>
+                <h4>Don't see the right opening for you?</h4>
+                <p>Send us your resume and we will be in touch! <a href="#">Contact Us Today</a></p>
 			</div>
 		</div>
 	</div>
