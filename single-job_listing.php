e1bdf236ca299cb4a68c811b33289e9666ac822d
Frost <cpaccione77@gmail.com>
2018-03-28 10:56:48 -0500

    CSS updates

--- single-job_listing.php
+++ single-job_listing.php
@@ -9,33 +9,32 @@
 
 get_header(); ?>
 
-<!-- <div id="primary" class="content-area">
-	<main id="main" class="site-main"> -->
-	<div class="container">
-		<div class="row">
+	<section class="single-job-container">
+		<div class="container">
+			<div class="row">
 
-			<div class="col-sm-8">
-				<?php
-					while ( have_posts() ) : the_post();
+				<div class="col-sm-12">
+					<div class="single-job-wrap">
+						<?php
+							while ( have_posts() ) : the_post();
 
-						get_template_part( 'template-parts/content', get_post_type() );
+								get_template_part( 'template-parts/content', get_post_type() );
 
-						the_post_navigation();
+								the_post_navigation();
 
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
+									comments_template();
+								endif;
 
-					endwhile; // End of the loop.
-				?>
+							endwhile; // End of the loop.
+						?>
+					</div>
+
+				</div>
+
 			</div>
-
-			<div class="col-sm-4">
-				<?php get_sidebar('job'); ?>
-			</div>
-
 		</div>
-	</div>
+	</section>
 
 <?php get_footer(); ?>
