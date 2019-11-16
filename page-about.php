76fe4c2f9d265d2007a4823e126b89255b280114
Chris Paccione <cpaccione77@gmail.com>
2019-11-16 14:01:24 -0600

    About page -adding ACF fields

--- page-about.php
+++ page-about.php
@@ -15,10 +15,18 @@ Template Name: About Page
 				<div class="container">
 					<div class="row">
 						<div class="col-md-7">
-							<h1>about</h1>
-							<p class="sub-sections">We offer and support contract, contract-to-hire, and direct hire staffing needs.</p>
-							<p>Eclipse is a Small-Women owned business with Executive staff providing a combined 25+ years of experience in the wireless and technology industries. Our range includes staffing, services, and vendor management with a focus on IT, Engineering, and Project/Program Management.</p>
-							<p>We offer flexible solutions and place significant emphasis on automation to keep our rates and fees in line with the industry (or lower). We understand and agree that each business is unique and will work hard to not only earn, but maintain your business.</p>
+							<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+								<?php the_title('<h1>', '</h1>'); ?>
+								
+								<?php the_content(); ?>
+
+								<?php endwhile; else: ?>
+
+								<h1>Oh no!</h1>
+								<p>No content is appearing on this page!</p>
+
+							<?php endif; ?>
 						</div>
 						<div class="col-md-5 d-md-flex align-items-md-center">
 							<img class="mx-auto d-block wob-logo" src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/sba_wosb_vertical.svg" alt="SBA Women owned business logo">
@@ -32,7 +40,7 @@ Template Name: About Page
 
 							<div class="row">
 								<div class="col-12">
-									<h2 class="team-heading">meet the team</h2>
+									<h2 class="team-heading"><?php the_field('team_section_title'); ?></h2>
 								</div>
 							</div>
 
@@ -57,7 +65,7 @@ Template Name: About Page
 												<div class="copy-wrap">
 													<h3><?php echo $name_t; ?></h3>
 													<h4><?php echo $title_t; ?></h4>
-													<p><?php echo $about_t; ?></p>
+													<p><?php //echo $about_t; ?></p>
 													<!-- modal button -->
 													<a class="link-button" data-toggle="modal" data-target="#<?php echo $link_t; ?>">learn more</a>
 												</div>
@@ -69,7 +77,6 @@ Template Name: About Page
 											<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
 												<div class="modal-content">
 													<div class="modal-header">
-														<h3 class="modal-title"><?php echo $name_t; ?></h3>
 														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 															<span aria-hidden="true">&times;</span>
 														</button>
@@ -77,12 +84,15 @@ Template Name: About Page
 													<div class="modal-body">
 														<div class="container">
 															<div class="row">
-																<div class="col-5">
-																	<img src="<?php echo $photo_t['url'] ?>" alt="<?php echo['alt']; ?>">
-																</div>
-																<div class="col-7">
-																	<h4><?php echo $title_t; ?></h4>
-																	<p><?php echo $about_t; ?></p>
+																<div class="col-md-12">
+																	<div class="modal-profile-text">
+																		<img src="<?php echo $photo_t['url']; ?>" alt="<?php echo['alt']; ?>">
+																	<!-- </div> -->
+																	<!-- <div class="col-md-12"> -->
+																	<h3 class="modal-title"><?php echo $name_t; ?></h3>
+																		<h4><?php echo $title_t; ?></h4>
+																		<p><?php echo $about_t; ?></p>
+																	</div>
 																</div>
 															</div>
 														</div>
@@ -101,47 +111,37 @@ Template Name: About Page
 		</div> <!-- container -->
 	</div> <!-- meet the team -->
 
-	<div class="our-partners d-sm-flex align-items-sm-center">
+	<!-- <div class="our-partners d-sm-flex align-items-sm-center"> -->
+	<div class="about-section-logos">
+
 		<div class="container">
+
 			<div class="row">
 				<div class="col-12">
-					<h2>our partners</h2>
+					<h2><?php the_field('logo_section_title'); ?></h2>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-4">
-					<div class="image-wrap mx-auto d-flex">
-						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/verizon.svg" alt="Verizon logo" />
+
+			<?php if( have_rows('logo_repeater') ): ?>
+
+					<div class="logos">
+
+						<?php while( have_rows('logo_repeater') ): the_row(); 
+
+							// vars
+							$image = get_sub_field('logo');
+
+							?>
+
+								<div class="logo-col">
+									<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+								</div>
+
+						<?php endwhile; ?>
+						
 					</div>
-				</div>
-				<div class="col-4">
-					<div class="image-wrap mx-auto d-flex">
-						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/samsung.svg" alt="Verizon logo" />
-					</div>
-				</div>
-				<div class="col-4">
-					<div class="image-wrap mx-auto d-flex">
-						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/sprint.svg" alt="Verizon logo" />
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-4">
-					<div class="image-wrap mx-auto d-flex">
-						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/extenet.svg" alt="Verizon logo" />
-					</div>
-				</div>
-				<div class="col-4">
-					<div class="image-wrap mx-auto d-flex">
-						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/teleworld.svg" alt="Verizon logo" />
-					</div>
-				</div>
-				<div class="col-4">
-					<div class="image-wrap mx-auto d-flex">
-						<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/crowncastle.svg" alt="Verizon logo" />
-					</div>
-				</div>
-			</div>
+
+				<?php endif; ?>
 		</div>
 	</div>
 
