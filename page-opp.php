0874b438f7f094c522422aeefed3f27b8877824b
Chris Paccione <cpaccione77@gmail.com>
2019-11-20 23:55:01 -0600

    Opportunities template and typography
    - Added ACF to opportunities template
    - Updated font sizes

--- page-opp.php
+++ page-opp.php
@@ -14,11 +14,8 @@ Template Name: Opportunities
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<?php the_title('<h1>', '</h1>'); ?>
-                <h4>We'd love to hear from you!</h4>
-				<p>When you apply for a job opening with Eclipse, you’re getting the attention you deserve. Whether or not you’re chosen for the opening, we will continue to work with you to find a spot for you!</p>
-                <h4>Don't see the right opening for you?</h4>
-                <p>Send us your resume and we will be in touch! <a href="#">Contact Us Today</a></p>
+                <?php the_title('<h1>', '</h1>'); ?>
+                <?php the_field('hero_copy'); ?>
 			</div>
 		</div>
 	</div>
@@ -28,18 +25,7 @@ Template Name: Opportunities
     <div class="container">
         <div class="row">
             <div class="col-12">
-            <?php if ( have_posts() ) :
-
-                /* Start the Loop */
-                while ( have_posts() ) : the_post();
-
-                the_content();
-
-                endwhile;
-
-                endif;
-
-            ?>
+                <?php the_field('job_search_editor'); ?>
             </div>
         </div>
     </div>
