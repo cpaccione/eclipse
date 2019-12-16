19ccc35cf765d84f0a767a6a539821a4fd1dee7f
Chris Paccione <cpaccione77@gmail.com>
2019-12-15 21:35:38 -0600

    Contact page update

--- page-contact.php
+++ page-contact.php
@@ -41,9 +41,11 @@ Template Name: Contact
 				</div>
 			</div>
 			<div class="col-md-6">
-				<div style="height: 75%;" id="map"></div>
-				<p>Eclipse Global Resources</p>
+				<div style="height: 400px;" id="map"></div>
+				<h4 style="color: #00c0f2; font-weight: bold; margin-top: 1rem;">Eclipse Global Resources</h4>
 				<p>HQ: Chicago, IL</p>
+				<p><i class="fas fa-mobile-alt"></i> 773-991-0818</p>
+				<p><i class="fas fa-envelope"></i> <a href="mailto:sales@eclipsellc.com" target="_top">sales@eclipsellc.com</a></p>
 			</div>
 		</div>
 	</div>
@@ -53,10 +55,231 @@ Template Name: Contact
       // Initialize and add the map
       function initMap() {
         // The location of Uluru
-        var chicago = {lat: 41.8781722, lng: -87.6649034};
+        var chicago = {
+			lat: 41.8781722,
+			lng: -87.6649034,
+			};
         // The map, centered at Uluru
         var map = new google.maps.Map(
-            document.getElementById('map'), {zoom: 8, center: chicago});
+            document.getElementById('map'), {
+				zoom: 8,
+				center: chicago,
+				mapTypeControl: false,
+				streetViewControl: false,
+				scrollwheel: false,
+				styles: [
+						{
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#f5f5f5"
+							}
+							]
+						},
+						{
+							"elementType": "labels.icon",
+							"stylers": [
+							{
+								"visibility": "on"
+							}
+							]
+						},
+						{
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#616161"
+							}
+							]
+						},
+						{
+							"elementType": "labels.text.stroke",
+							"stylers": [
+							{
+								"color": "#f5f5f5"
+							}
+							]
+						},
+						{
+							"featureType": "administrative.land_parcel",
+							"stylers": [
+							{
+								"visibility": "off"
+							}
+							]
+						},
+						{
+							"featureType": "administrative.land_parcel",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#bdbdbd"
+							}
+							]
+						},
+						{
+							"featureType": "administrative.neighborhood",
+							"stylers": [
+							{
+								"visibility": "off"
+							}
+							]
+						},
+						{
+							"featureType": "poi",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#eeeeee"
+							}
+							]
+						},
+						{
+							"featureType": "poi",
+							"elementType": "labels.text",
+							"stylers": [
+							{
+								"visibility": "off"
+							}
+							]
+						},
+						{
+							"featureType": "poi",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#757575"
+							}
+							]
+						},
+						{
+							"featureType": "poi.park",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#e5e5e5"
+							}
+							]
+						},
+						{
+							"featureType": "poi.park",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#9e9e9e"
+							}
+							]
+						},
+						{
+							"featureType": "road",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#ffffff"
+							}
+							]
+						},
+						{
+							"featureType": "road",
+							"elementType": "labels",
+							"stylers": [
+							{
+								"visibility": "off"
+							}
+							]
+						},
+						{
+							"featureType": "road.arterial",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#757575"
+							}
+							]
+						},
+						{
+							"featureType": "road.highway",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#dadada"
+							}
+							]
+						},
+						{
+							"featureType": "road.highway",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#616161"
+							}
+							]
+						},
+						{
+							"featureType": "road.local",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#9e9e9e"
+							}
+							]
+						},
+						{
+							"featureType": "transit.line",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#e5e5e5"
+							}
+							]
+						},
+						{
+							"featureType": "transit.station",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#eeeeee"
+							}
+							]
+						},
+						{
+							"featureType": "water",
+							"elementType": "geometry",
+							"stylers": [
+							{
+								"color": "#c9c9c9"
+							}
+							]
+						},
+						{
+							"featureType": "water",
+							"elementType": "geometry.fill",
+							"stylers": [
+							{
+								"color": "#00c0f2"
+							}
+							]
+						},
+						{
+							"featureType": "water",
+							"elementType": "labels.text",
+							"stylers": [
+							{
+								"visibility": "off"
+							}
+							]
+						},
+						{
+							"featureType": "water",
+							"elementType": "labels.text.fill",
+							"stylers": [
+							{
+								"color": "#00c0f2"
+							}
+							]
+						}
+						]
+				});
         // The marker, positioned at Uluru
         var marker = new google.maps.Marker({position: chicago, map: map});
         }
